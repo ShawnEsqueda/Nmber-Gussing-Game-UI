5185c7bc1d259b025889e13aa64eb472e74a4473
ShawnEsqueda <shawnesqueda@icloud.com>
2022-09-02 11:18:03 -0700

    Finished Project

---
+++ Assets/scripts/GamePlay.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GamePlay : MonoBehaviour
+{
+  
+  private int guess;
+  [SerializeField] private int minValue;
+  [SerializeField] private int maxValue;
+
+  [SerializeField] private TextMeshProUGUI guessText;
+
+  // Start is called before the first frame update
+
+  void Start() 
+  {
+      StartGame();
+    }
+
+    public void OnPressHigher()
+    {
+      minValue = guess = 1;
+      NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+      maxValue = guess - 1;
+      NextGuess();
+    }
+
+    public void NextGuess()
+    {
+      guess = Random.Range(minValue, maxValue + 1);
+      guessText.text.ToString();
+    }
+
+    void StartGame()
+    {
+      NextGuess();
+    }
+  }
+
